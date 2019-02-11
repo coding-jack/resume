fe11c081dd428311a92d100be611215660abb7fe
Jackson Kaufman <jacksonkaufman115@gmail.com>
2019-02-11 13:33:22 -0700

    Initial commit with dependencies installed, bare bones

---
+++ src/Resume.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="resume" class="container py-3 py-lg-5">
+    <b-row>
+      <b-col lg="2" order-lg="2" class="nav-area ml-auto pb-3">
+        <main-nav/>
+      </b-col>
+      <b-col md="12" lg="9" class="main">
+        <transition name="fade" transition-mode="in-out">
+          <router-view></router-view>
+        </transition>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Resume'
+}
+</script>
+
+<style>
+  @import './assets/sass/resume.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// import Resume from './Resume'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import Resume from './Resume'
+// import router from './router'
+import './assets/sass/resume.scss'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import './svg-triangles'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTerminal, faMobile, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.use(BootstrapVue)
+// Vue.use(require('vue-moment'))
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#resume',
+  router,
+  components: { Resume },
+  template: '<Resume/>'
+})
